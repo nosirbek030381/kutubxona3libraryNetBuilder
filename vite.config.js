71101d6ab05d9c4987088ef2f83d8b094af58e70
nosirbek030381 <ismoilovn899@gmail.com>
2023-09-24 19:39:50 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import reactRefresh from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRefresh()],
+  base: '/build', 
+  server: {
+    port: 5173, 
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+    }
+  }
 })
