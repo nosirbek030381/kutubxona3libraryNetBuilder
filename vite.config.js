01ab689d9ff5b0c51a311770dcdb0e0f527a7139
nosirbek030381 <ismoilovn899@gmail.com>
2023-09-24 19:43:21 +0500

    .1

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  base: '/build', 
-  server: {
-    port: 5173, 
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-    }
-  }
+  plugins: [react()],
 })
