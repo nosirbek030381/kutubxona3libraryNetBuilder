09b26428927ea922d31d07f5f9397f46e26be2da
nosirbek030381 <ismoilovn899@gmail.com>
2023-09-24 19:41:26 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import About from './components/aboutUs/About';
 import BookDetail from './components/bookDetail/bookDetail';
 import Books_List from './components/books/books';
-import Home from './components/pages/Home';
+import Home from './components/pages/home';
 
 function App() {
 	return (
